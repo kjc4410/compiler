f8cc288fb51dfb0a7ef13d6b3ca7f1e1b096d22d
kjc4410 <87844641+kjc4410@users.noreply.github.com>
2023-11-15 01:17:41 +0900

    Update follow.cpp

--- Front End/follow.cpp
+++ Front End/follow.cpp
@@ -6,19 +6,20 @@
 #include<set>
 #include<vector>
 #include<unordered_map>
+#include<unordered_set>
 using namespace std;
 
 /*
-    Grammar
-    1. Nonterminal은 대문자로 작성
-    2. Terminal은 소문자로 작성
-    3. 유도는 ->로 나타냄
-    4. 문법의 lhs가 같으면 | 기호를 통해 문법 규칙을 나열할 수 있음
+	Grammar
+	1. Nonterminal은 대문자로 작성
+	2. Terminal은 소문자로 작성
+	3. 유도는 ->로 나타냄
+	4. 문법의 lhs가 같으면 | 기호를 통해 문법 규칙을 나열할 수 있음
 */
 
 vector<string> Grammar;
 enum Tokens {
-	Nonterminal=0,
+	Nonterminal = 0,
 	Terminal,
 	Operator,
 };
@@ -28,6 +29,7 @@ void FOLLOW(string s);
 void Processing(string s);
 
 string arrow = "->";
+string starting;
 using Head = string;
 using Body = string;
 using BodyToken = vector<pair<string, Tokens>>;
@@ -39,13 +41,11 @@ vector<pair<string, BodyToken>> OnlyBody;
 // head는 생성규칙의 lhs 
 unordered_multimap<Head, BodyToken> Rules;
 
-set<string> NontermTable;
-set<string> TermTable;
+unordered_set<string> NontermTable;
+unordered_set<string> TermTable;
 
 unordered_map<string, vector<string>> FIRST_SET;
-unordered_map<string, vector<string>> FOLLOW_SET; 
-//unordered_map<string, string> First1;
-//vector< pair<string, vector<string>> > First2;
+unordered_map<string, vector<string>> FOLLOW_SET;
 
 //FIRST를 구하는 함수
 void FIRST(string nonterminal) {
@@ -54,101 +54,125 @@ void FIRST(string nonterminal) {
 
 	//unordered_multimap에서 중복되는 값들을 찾는 과정
 	for (auto it = range.first; it != range.second; it++) {
-
 		// vector<pair<string, Tokens>>에 접근
 		BodyToken vec = it->second;
 
 		// 생성규칙의 첫 번째가 terminal이면 이를 추가
 		if (vec[0].second == Terminal)
 			v.push_back(vec[0].first);
-
-		else{ // nonterminal이면 해당 nonterminal의 first를 구함
+		else { // nonterminal이면 해당 nonterminal의 first를 구함
 			FIRST(vec[0].first); //재귀를 통해 first를 구하고
-			if(!FIRST_SET[vec[0].first].empty()) 
-				for(auto &i : FIRST_SET[vec[0].first]) 
+			if (!FIRST_SET[vec[0].first].empty())
+				for (auto& i : FIRST_SET[vec[0].first])
 					v.push_back(i);
 		}
 	}
-	FIRST_SET.insert({nonterminal,v});
+	FIRST_SET.insert({ nonterminal,v });
 }
 
-unordered_map<string,bool> visited;
-
-void visited_init(){
-	for(auto &i: NontermTable)
-		visited[i]=false;
-}
-
-void FOLLOW(string nonterminal){
-	vector<string> vv;
-	for(auto& i : OnlyBody){
-		BodyToken rule_body = i.second;
-		string rule_name = i.first;
-		for(int j=0;j<rule_body.size();j++){
-			
-			if(rule_body[j].first == nonterminal){ //규칙의 rhs의 토큰이라면
-				string cur_token = rule_body[j].first;
-				string next_token = rule_body[j+1].first;
-				//찾은 rhs의 다음 토큰이 없으면 현재 FOLLOW에 lhs의 terminal을 넣음
-				if( j+1 == rule_body.size()){
-					
-					if(rule_name == cur_token)
-						continue;
-
-					//FOLLOW(rule_name);
-
-					if(!FOLLOW_SET[rule_name].empty())
-						for(auto &k : FOLLOW_SET[rule_name])
-							vv.push_back(k);
-					continue;
-				}
-				if(rule_body[j+1].second ==Terminal){ // 그 다음 토큰이 terminal이면
-					
-					//if(find(FOLLOW_SET[next_token].begin(), FOLLOW_SET[next_token].end(),   )
-					
-					vv.push_back(next_token); //벡터에 삽입
-				}
-				else if(rule_body[j+1].second == Nonterminal){ // 그 다음 토큰이 nonterminal이라면
-					//if(FIRST_SET[rule_body[j+1].first] == FOLLOW_SET[nonterminal])
-
-
-					// 1. 만약 규칙들 중 같은 Nonterminal이 반복적으로 있으면 굳이 일일이 하나씩 검증할 필요 없이 방문을 통해 중복 부분 탐색 x
-					// 2. 아니면 중복되는 것을 모두 실행하여 벡터에 넣고 벡터의 중복 원소 제거 방법으로 다 없애기 
-					if(visited[next_token] ==true)
-						continue;
-
-
-					for(auto &k : FIRST_SET[next_token]){ // 그 nonterminal의 first를 구함
-						if(k != "epsilon")
-							vv.push_back(k);
+void FOLLOW1() {
+	for (auto& nonterm : NontermTable) {
+		for (auto& i : OnlyBody) {
+			BodyToken rule_body = i.second;
+			string rule_name = i.first;
+			for (int j = 0; j < rule_body.size(); j++) {
+				if (rule_body[j].first == nonterm && j + 1 != rule_body.size()) {
+					if (rule_body[j + 1].second == Nonterminal) {
+						for (auto& k : FIRST_SET[rule_body[j + 1].first])
+							if (k != "epsilon")
+								if (find(FOLLOW_SET[nonterm].begin(), FOLLOW_SET[nonterm].end(), k) == FOLLOW_SET[nonterm].end()) {
+									FOLLOW_SET[nonterm].push_back(k);
+								}
 					}
-					visited[next_token]=true;
-					
+					else if (rule_body[j + 1].second == Terminal)
+						if (find(FOLLOW_SET[nonterm].begin(), FOLLOW_SET[nonterm].end(), rule_body[j + 1].first) == FOLLOW_SET[nonterm].end()) {
+							FOLLOW_SET[nonterm].push_back(rule_body[j + 1].first);
+						}
 				}
 			}
 		}
 	}
+}
+void FOLLOW2() {
+	bool updated = true;
+	while (updated) {
+		updated = false;
+		for (auto& nonterm : NontermTable) {
+			for (auto& i : OnlyBody) {
+				BodyToken rule_body = i.second;
+				string rule_name = i.first;
+				for (int j = 0; j < rule_body.size(); j++) {
 
-	//아래는 벡터 중복 제거 원소 
-	//sort(vv.begin(), vv.end());
-	//vv.erase(unique(vv.begin(), vv.end()), vv.end());
-	//FOLLOW_SET.insert(make_pair(s, vv)); <-----이거는 벡터에 적용이 안됨
-	FOLLOW_SET[nonterminal]=vv;
-	return;
+					if (rule_body[j].first == nonterm && j + 1 == rule_body.size()) { //규칙에서 nonterminal 찾기
+						string cur = rule_body[j].first;
+
+
+						for (auto& k : FOLLOW_SET[rule_name]) {
+
+							if (find(FOLLOW_SET[cur].begin(), FOLLOW_SET[cur].end(), k) == FOLLOW_SET[cur].end()) {
+								FOLLOW_SET[cur].push_back(k);
+								updated = true;
+							}
+						}
+
+
+						// 맨 뒤의 noterminal들의 first를 확인하고 nullable할 때 이전 토큰이 nonterminal인지 확인하고 
+						// 맞다면 이전 nonterminal에도 head의 follow 추가하고 다시 이전 nonterminal의 first 검사 <--- 이를 반복 
+						int cnt = 0;
+						while (cnt < rule_body.size()) {
+							bool isEpsilon = false;
+							for (auto& k : FIRST_SET[rule_body[j - cnt].first])
+								if (k == "epsilon") {
+
+									isEpsilon = true;
+									cnt++;
+									if (rule_body[j - cnt].second == Nonterminal) {
+
+										string back = rule_body[j - cnt].first;
+										for (auto& k : FOLLOW_SET[rule_name]) {
+											if (find(FOLLOW_SET[back].begin(), FOLLOW_SET[back].end(), k) == FOLLOW_SET[back].end()) {
+												FOLLOW_SET[back].push_back(k);
+												updated = true;
+											}
+										}
+									}
+									else
+										break;
+								}
+							if (isEpsilon == false)
+								break;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+void FOLLOW() {
+	FOLLOW_SET[starting].push_back("$");
+	FOLLOW1();
+
+		FOLLOW2();
+	for (auto& i : NontermTable) {
+
+	}
+		
 }
 
-
 void Processing() {
+	int counting = 0;
 	for (auto& line : Grammar) {
 		int index = line.find(arrow);
 
 		string LHS;
-		for (int i = 0;i < index;i++)
+		for (int i = 0; i < index; i++)
 			if (line[i] != ' ')
 				LHS += line[i];
 		NontermTable.insert(LHS);
+		if (counting == 0)
+			starting = LHS;
 		//NontermTable.push_back(LHS);
-	
+
 		string RHS = line.substr(index + 2, line.size() - index + 2);
 
 		istringstream rhs(RHS);
@@ -184,9 +208,10 @@ void Processing() {
 				}
 				body.push_back(make_pair(token, kind));
 			}
-			Rules.insert({LHS, body});
-			OnlyBody.push_back({LHS ,body});
+			Rules.insert({ LHS, body });
+			OnlyBody.push_back({ LHS ,body });
 		}
+		counting++;
 	}
 	std::cout << "--Rule-- \n";
 	for (auto& i : Rule)
@@ -197,9 +222,9 @@ void Processing() {
 
 	std::cout << "Terminal 종류: \n";
 	for (auto& i : TermTable)
-		std::cout <<i<<" " << endl;
+		std::cout << i << " " << endl;
 	std::cout << endl;
-	
+
 	std::cout << "Nonterminal 종류: \n";
 	for (auto& i : NontermTable)
 		std::cout << i << " " << endl;
@@ -221,29 +246,31 @@ int main() {
 
 	Processing();
 
-	cout<<"\n------FIST------\n";
+	std::cout << "\n------FIST------\n";
 	for (auto& i : NontermTable) {
 		std::cout << "FIRST(" << i << ")= { ";
 		FIRST(i);
-		for(auto &j : FIRST_SET[i])
-			cout<<j<<" ";	
+		for (auto& j : FIRST_SET[i])
+			std::cout << j << " ";
 		std::cout << "}\n";
 	}
-	
-	cout<<"\n-----FOLLOW-----\n";
-    for (auto& i : NontermTable){
-        std::cout << "FOLLOW(" << i <<")= { ";
-        FOLLOW(i);
-		for(auto &j : FOLLOW_SET[i])
-			cout << j << " ";
-        std::cout<<"}\n";
-    }
-	
+
+	FOLLOW();
+
+	std::cout << "\n-----FOLLOW-----\n";
+	for (auto& i : NontermTable) {
+		std::cout << "FOLLOW(" << i << ")= { ";
+
+		for (auto& j : FOLLOW_SET[i])
+			std::cout << j << " ";
+		std::cout << "}\n";
+
+	}
 	return 0;
 }
 
 bool isOperator(char ch) {
-	switch(ch){
+	switch (ch) {
 	case '+': case '*': case '-': case '/':
 	case '%': case'(': case ')': case'#':
 		return true;
