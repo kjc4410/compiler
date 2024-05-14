a52033c6accd12a5341c6a61021935bbd365eadf
kjc4410 <kjc4410@naver.com>
2024-05-14 18:18:50 +0900

    update DF

--- CFG/Dominator.cpp
+++ CFG/Dominator.cpp
@@ -121,21 +121,24 @@ int main() {
     //--6--
 
    CF[1].push_back(2);
-   CF[1].push_back(3);
-   CF[2].push_back(3);
-   CF[3].push_back(4);
-   CF[4].push_back(3);
-   CF[4].push_back(5);
-   CF[4].push_back(6);
-   CF[5].push_back(7);
-   CF[6].push_back(7);
-   CF[7].push_back(4);
-   CF[7].push_back(8);
-   CF[8].push_back(9);
-   CF[8].push_back(10);
-   CF[8].push_back(3);
-   CF[9].push_back(1);
-   CF[10].push_back(7);
+    CF[1].push_back(14);
+    CF[2].push_back(3);
+    CF[2].push_back(4);
+    CF[3].push_back(5);
+    CF[3].push_back(6);
+    CF[3].push_back(7);
+    CF[4].push_back(8);
+    CF[4].push_back(9);
+    CF[4].push_back(10);
+    CF[5].push_back(11);
+    CF[6].push_back(11);
+    CF[7].push_back(11);
+    CF[8].push_back(12);
+    CF[9].push_back(12);
+    CF[10].push_back(12);
+    CF[11].push_back(13);
+    CF[12].push_back(13);
+    CF[14].push_back(13);
     //----------- Dominator 트리 계산---------//
     Dom(entry);  // 시작 노드를 1로 가정
 
@@ -182,8 +185,7 @@ int main() {
 
         parent = i;
         dfs(i);
-        if (Frontier[i].empty())
-            Frontier[i].push_back(-1);//공집한은 -1로 표현
+
         memset(visited,false, node_size+1);
         memset(checking, false, node_size + 1);
         cout << "을 방문\n";
@@ -193,9 +195,7 @@ int main() {
     for (int i = 1;i <= node_size;i++) {
         cout << "DF(" << i << "): ";
         for (int j = 0;j < Frontier[i].size();j++) {
-            if (Frontier[i][j] == -1)
-                cout << "x"; // 공집합
-            else
+            if (!Frontier[i].empty())
                 cout << Frontier[i][j] << " ";
         }
         cout << endl;
@@ -213,7 +213,7 @@ int main() {
             if(Frontier[i].empty())
                 cout<<"x";
             else
-                cout<<"Frontier[i][j]"<<" ";
+                cout<<Frontier[i][j]<<" ";
         }
         cout<<endl;
     }
